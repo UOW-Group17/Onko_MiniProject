8478cad9cfa604678fd7faaa595ba50ab037cadb
mattc <mattcarlile590@gmail.com>
2025-04-25 15:53:48 +1000

    Improvements to the Interface Class #2879

--- src/InterfaceUserPref.py
+++ src/InterfaceUserPref.py
@@ -2,24 +2,24 @@
 To provide a set group of methods for other classes
 to access the UserPreferences Class
 """
+import pathlib
 from abc import ABC, abstractmethod
 
 class InterfaceUserPref(ABC):
     """
-    Abstract class to provide methods the to define
-    the accessible methods of the UserPreferences Class
+    Abstract class to define methods to
+    access the UserPreferences Class
     """
     @abstractmethod
-    def check_if_path_exists(self) -> bool:
-        """ Check if there exists a saved path """
-        pass
-
-    @abstractmethod
-    def save_default_path(self, path:str) -> None:
+    def save_default_path(self, path:pathlib.Path) -> None:
         """ Saving a default path to the database """
         pass
 
     @abstractmethod
-    def load_default_path(self) -> str:
-        """ Getting a default path from the database"""
+    def default_path(self) -> pathlib.Path | None:
+        """
+        Getting a default path from the database
+        A check is likely Required before using this method
+        to ensure that a default path exists
+        """
         pass
