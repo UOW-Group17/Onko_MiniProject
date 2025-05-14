1c4035e2f8612bb9c28ba2168bad7d66fadd6186
mattc <mattcarlile590@gmail.com>
2025-05-14 18:10:42 +1000

    formatting changes to add_directory()

--- src/mini_project_ui.py
+++ src/mini_project_ui.py
@@ -111,7 +111,9 @@ class MiniProjectUI(QtWidgets.QDialog):
 
     def add_directory(self):
         """Function for the button click""" 
-        dir_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, caption="Select DICOM File", dir=str(pathlib.Path(self.path).parent),
+        dir_path, _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                            caption="Select DICOM File",
+                                                            dir=str(pathlib.Path(self.path).parent),
                                                             filter="DICOM Files (*.dcm)")
         self.path = dir_path
         self.open_dicom_file()
